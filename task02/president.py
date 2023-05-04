93ac392f01c393dab24f09b2b2e6ab61c5b8ad2a
AlexPronkevich <aa-kk@yandex.ru>
2023-05-04 21:55:46 +0300

    Добавлен таск 02

---
+++ task02/president.py
@@ -0,0 +1,48 @@
+# Human - Entity class
+
+class President:
+    def __init__(self, name='no name', age=0, alive=True, power=10):
+        self.__name = name
+        self.__age = age
+        self.__alive = alive
+        self.__power = power
+
+    @property
+    def power(self):
+        return self.__power
+
+    @power.setter
+    def power(self, power):
+        if isinstance(power, int) and  0 < power <= 100:
+            self.__power = power
+
+    @property
+    def name(self):
+        return self.__name.capitalize()
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str):
+            self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age=1):
+        if 0 < age <= 120:
+            self.__age = age
+
+    @property
+    def is_alive(self):
+        return "Yes" if self.__alive else "No"
+
+    @is_alive.setter
+    def is_alive(self, alive):
+        if isinstance(alive, bool):
+            self.__alive = alive
+
+    def __str__(self):
+        return f"{self.name}: age = {self.__age}. " \
+               f"Is alive? - {self.is_alive}"
