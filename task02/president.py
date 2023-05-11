dcbad2e974f51eb85d3ec0cf34c0fe8a6e45a34b
AlexPronkevich <aa-kk@yandex.ru>
2023-05-11 20:52:07 +0300

    Используем базовый класс Human для дочерних классов President, Doctor, Worker, Student

--- task02/president.py
+++ task02/president.py
@@ -1,6 +1,6 @@
 # Human - Entity class
 
-class President:
+class President(Human):
     def __init__(self, name='no name', age=0, alive=True, power=10):
         self.__name = name
         self.__age = age
@@ -16,33 +16,3 @@ class President:
         if isinstance(power, int) and  0 < power <= 100:
             self.__power = power
 
-    @property
-    def name(self):
-        return self.__name.capitalize()
-
-    @name.setter
-    def name(self, name):
-        if isinstance(name, str):
-            self.__name = name
-
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, age=1):
-        if 0 < age <= 120:
-            self.__age = age
-
-    @property
-    def is_alive(self):
-        return "Yes" if self.__alive else "No"
-
-    @is_alive.setter
-    def is_alive(self, alive):
-        if isinstance(alive, bool):
-            self.__alive = alive
-
-    def __str__(self):
-        return f"{self.name}: age = {self.__age}. " \
-               f"Is alive? - {self.is_alive}"
--- task02/student.py
+++ task02/student.py
@@ -1,6 +1,6 @@
 # Student - Entity class
 
-class Student:
+class Student(Human):
     def __init__(self, name='no name', age=0, alive=True, mark=4):
         self.__name = name
         self.__age = age
@@ -19,34 +19,3 @@ class Student:
         if 0 <= mark <= 10:
             self.__mark = mark
 
-    @property
-    def name(self):
-        return self.__name.capitalize()
-
-    @name.setter
-    def name(self, name):
-        if isinstance(name, str):
-            self.__name = name
-
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, age=1):
-        if 0 < age <= 120:
-            self.__age = age
-
-    @property
-    def is_alive(self):
-        return "Yes" if self.__alive else "No"
-
-    @is_alive.setter
-    def is_alive(self, alive):
-        if isinstance(alive, bool):
-            self.__alive = alive
-
-    def __str__(self):
-        return f"{self.name}: age = {self.__age}. " \ 
-               f"mark ={self.__mark}"\
-               f"Is alive? - {self.is_alive}"
